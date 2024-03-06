41a46ee8005b93925467b7650f33880a8f71faf4
Simon Gomez Torres <saaimonsg2@gmail.com>
2024-03-05 23:01:30 -0300

    login stringify body for request

--- pages/login/index.js
+++ pages/login/index.js
@@ -11,37 +11,30 @@ export default function Login() {
     const router = useRouter()
     const [username, setUsername] = useState("string")
     const [password, setPassword] = useState("string")
-    useEffect(() => {
-
-    }, []);
 
     const handleSubmitForm = async (event) => {
         event.preventDefault();
-
+        // await fetch( `http://localhost:8080/login?username=${username}&password=${password}`, {
         await fetch(`http://localhost:8080/api/auth/login`, {
-            method: 'POST', headers: {
-                'Accept': 'application/json', 'Content-Type': 'application/json'
-            },
+            method: 'POST',
             body: JSON.stringify({
                 "username": username,
                 "password": password
-            }),
-            mode:"cors"
-
+            })
         }).then(value => {
-
+            // const jsessionid = value.headers.get("Set-Cookie")
+            // console.log(jsessionid)
             fetch(`http://localhost:8080/app/country`, {
-                method: 'POST', headers: {
-                    'Accept': 'application/json', 'Content-Type': 'application/json'
-                },
-                mode:"cors"
+                method: 'POST',
+                mode: "cors"
             });
+            // router.push("/dashboard")
         });
 
     }
 
     return (<>
-        <TopNavbar></TopNavbar>
+        {/*<TopNavbar></TopNavbar>*/}
         <form id='loginform' className="container" onSubmit={handleSubmitForm}>
             <div className="mb-3">
                 <label htmlFor="usernameInput" className="form-label">{t("usernameInput")}</label>
@@ -59,37 +52,7 @@ export default function Login() {
             </div>
             <input className="btn btn-primary" type='submit' value={t("login.tag")}/>
         </form>
-        <Footer></Footer>
     </>)
 }
 
 
-
-//OLD
-
-// axios.post(`http://localhost:8080/login?username=${username}&password=${password}`).then(value => {
-//     console.log(value)
-// })
-
-// const response = await fetch(`http://localhost:8080/login?username=${username}&password=${password}`, {
-//     method: 'POST', headers: {
-//         'Accept': 'application/json', 'Content-Type': 'application/json'
-//     }, mode: "cors"
-// }).then(response => {
-//     if (response.ok) {
-//         // Retrieve cookie from headers
-//         response.headers.forEach((name,value) => {
-//             console.log(value,name)
-//         })
-//         const cookieHeader = response.headers.get('Set-Cookie');
-//         // Extract specific cookie value if needed
-//         if (cookieHeader != undefined || cookieHeader != null) {
-//             const cookieValue = cookieHeader.split(';')[0]; // Assuming you only need the first cookie value
-//             console.log("Received cookie:", cookieValue);
-//         } else {
-//             console.log("NoCookie")
-//         }
-//     } else {
-//         console.error("Error occurred during login:", response.statusText);
-//     }
-// })
