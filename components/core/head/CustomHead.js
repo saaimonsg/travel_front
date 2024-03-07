e745972f50489c113eaa410d6e845eb6edeb1cdf
Simon Gomez Torres <saaimonsg2@gmail.com>
2024-03-07 01:58:17 -0300

    favicon.ico

--- components/core/head/CustomHead.js
+++ components/core/head/CustomHead.js
@@ -12,7 +12,7 @@ export default function CustomHead() {
             <meta charSet="utf-8"/>
             <title>{initDataContext.name}</title>
             <meta name="description" content={initDataContext.description}/>
-            <link rel="icon" href="./img/favicon.ico"/>
+            <link rel="icon" href="/img/favicon.ico"/>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
                   integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
                   crossOrigin="anonymous"/>
